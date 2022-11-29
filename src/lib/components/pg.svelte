c50993e742e4afba3e136041c81b1d7bba3a84dc
z <aolose@fsm.cc>
2022-11-30 00:06:07 +0800

    fix paging select

--- src/lib/components/pg.svelte
+++ src/lib/components/pg.svelte
@@ -1,105 +1,109 @@
 <script>
-	export let total;
-	export let page = 1;
-	export let go;
-	export let tm;
-	export let length = 4;
-	$: first = page === 1;
-	$: last = page === total;
-	let pg;
-	$: isF = typeof go === 'function';
+    export let total = 1;
+    export let page = 1;
+    export let go;
+    export let tm;
+    export let length = 4;
+    const setPage = n => () => {
+        go(n)
+        page = n
+    }
+    $: first = page === 1;
+    $: last = page === total;
+    let pg;
+    $: isF = typeof go === 'function';
 
-	$: {
-		pg = [];
-		if (2 < total) {
-			if (page < total && page > 1) pg = [page];
-			for (let i = 1; i <= length && pg.length < length; i++) {
-				const pr = page - i;
-				const nx = page + i;
-				if (pr > 1) pg = [pr, ...pg];
-				if (nx < total) pg = [...pg, nx];
-			}
-		}
-	}
-	$: f = pg[0] > 2 ? '...' : '';
-	$: n = pg[pg.length - 1] < total - 1 ? '...' : '';
+    $: {
+        pg = [];
+        if (2 < total) {
+            if (page < total && page > 1) pg = [page];
+            for (let i = 1; i <= length && pg.length < length; i++) {
+                const pr = page - i;
+                const nx = page + i;
+                if (pr > 1) pg = [pr, ...pg];
+                if (nx < total) pg = [...pg, nx];
+            }
+        }
+    }
+    $: f = pg[0] > 2 ? '...' : '';
+    $: n = pg[pg.length - 1] < total - 1 ? '...' : '';
 </script>
 
 <nav class:lt={tm}>
-	{#if isF}
-		<span class="nv" class:act={first} on:click={() => go(1)}>1</span>
-	{:else}
-		<a class="nv" class:act={first} href={`${go}/1`}>1</a>
-	{/if}
-	{#if f}<span>{f}</span>{/if}
-	{#each pg as p}
-		{#if isF}
-			<span class="nv" class:act={page === p} on:click={() => go(p)}>{p}</span>
-		{:else}
-			<a class="nv" class:act={page === p} href={`${go}/${p}`}>{p}</a>
-		{/if}
-	{/each}
-	{#if n}<span>{n}</span>{/if}
-	{#if total > 1}
-		{#if isF}
-			<span class="nv" class:act={last} on:click={() => go(total)}>{total}</span>
-		{:else}
-			<a class="nv" class:act={last} href={`${go}/${total}`}>{total}</a>
-		{/if}
-	{/if}
+    {#if isF}
+        <span class="nv" class:act={first} on:click={setPage(1)}>1</span>
+    {:else}
+        <a class="nv" class:act={first} href={`${go}/1`}>1</a>
+    {/if}
+    {#if f}<span>{f}</span>{/if}
+    {#each pg as p}
+        {#if isF}
+            <span class="nv" class:act={page === p} on:click={setPage(p)}>{p}</span>
+        {:else}
+            <a class="nv" class:act={page === p} href={`${go}/${p}`}>{p}</a>
+        {/if}
+    {/each}
+    {#if n}<span>{n}</span>{/if}
+    {#if total > 1}
+        {#if isF}
+            <span class="nv" class:act={last} on:click={setPage(total)}>{total}</span>
+        {:else}
+            <a class="nv" class:act={last} href={`${go}/${total}`}>{total}</a>
+        {/if}
+    {/if}
 </nav>
 
 <style lang="scss">
-	nav {
-		align-items: center;
-		justify-content: center;
-		display: flex;
+  nav {
+    align-items: center;
+    justify-content: center;
+    display: flex;
 
-		* {
-			color: rgb(78, 96, 115);
-		}
+    * {
+      color: rgb(78, 96, 115);
+    }
 
-		.act {
-			background: var(--bg0);
-		}
+    .act {
+      background: var(--bg0);
+    }
 
-		.nv {
-			font-size: 12px;
-			cursor: pointer;
-			height: 24px;
-			min-width: 24px;
-			padding: 0 5px;
-			display: flex;
-			margin: 0 5px;
-			justify-content: center;
-			align-items: center;
+    .nv {
+      font-size: 12px;
+      cursor: pointer;
+      height: 24px;
+      min-width: 24px;
+      padding: 0 5px;
+      display: flex;
+      margin: 0 5px;
+      justify-content: center;
+      align-items: center;
 
-			&:hover {
-				color: #65b9e7;
-			}
-		}
-	}
+      &:hover {
+        color: #65b9e7;
+      }
+    }
+  }
 
-	.lt {
-		* {
-			color: #1c334a;
-		}
+  .lt {
+    * {
+      color: #1c334a;
+    }
 
-		.nv {
-			color: #000;
-			background: none;
-			padding: 5px 8px;
-			height: 24px;
-			width: auto;
-			border-radius: 2px;
-			border: 1px solid currentColor;
-		}
+    .nv {
+      color: #000;
+      background: none;
+      padding: 5px 8px;
+      height: 24px;
+      width: auto;
+      border-radius: 2px;
+      border: 1px solid currentColor;
+    }
 
-		.act,
-		.nv:hover {
-			color: #fff;
-			border-color: #000;
-			background: #000;
-		}
-	}
+    .act,
+    .nv:hover {
+      color: #fff;
+      border-color: #000;
+      background: #000;
+    }
+  }
 </style>
