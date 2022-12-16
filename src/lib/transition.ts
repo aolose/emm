f5c331b78e355bf366a3e2176e1c1a438bee4700
z <aolose@fsm.cc>
2022-12-17 00:57:13 +0800

    trans

--- src/lib/transition.ts
+++ src/lib/transition.ts
@@ -1,4 +1,4 @@
-import {cubicOut} from 'svelte/easing'
+import {cubicOut, quadInOut} from 'svelte/easing'
 
 export const slidLeft = (node: Element, {delay = 0, duration = 400, easing = cubicOut}) => {
     const style = getComputedStyle(node);
@@ -23,5 +23,21 @@ export const slidLeft = (node: Element, {delay = 0, duration = 400, easing = cub
             `margin-right: ${t * margin_right}px;` +
             `border-left-width: ${t * border_left_width}px;` +
             `border-right-width: ${t * border_right_width}px;`
+    }
+}
+
+export function jump(node: Element, {
+    y = 20,
+    duration = 200,
+    easing = quadInOut
+}) {
+    const style = getComputedStyle(node);
+    const [, a, b, c, d, e, f] = (style.transform.match(/matrix\(([-0-9.]+), ([-0-9.]+), ([-0-9.]+), ([-0-9.]+), ([-0-9.]+), ([-0-9.]+)\)/) || []).map(a => +a)
+    return {
+        duration,
+        easing,
+        css: (t: number) => {
+            return `transform: matrix(${a}, ${b}, ${c}, ${d}, ${e}, ${f + (1 - t) * y})`
+        }
     }
 }
