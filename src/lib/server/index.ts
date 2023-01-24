83232ad84409b88e769ad28e0e68a48c641c4f46
z <aolose@fsm.cc>
2023-01-24 23:32:51 +0800

    load publishedPost

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -2,7 +2,7 @@ import {DB} from './db/sqlite3';
 import {runJobs} from './db/jobs';
 import {DBProxy, val} from './utils';
 import {System, Tag} from './model';
-import {tags} from "$lib/store";
+import {publishedPost, tags} from "$lib/store";
 import {loadGeoDb} from "$lib/server/ipLite";
 
 export let sys: System;
@@ -30,6 +30,9 @@ export const server = {
     sync() {
         const ts = db.db.prepare('select * from Tag order by createAt desc').all()
         tags.set(ts.map(a => DBProxy(Tag, a, false)))
+        publishedPost.set(new Set(
+            db.db.prepare('select id from Post where published =?')
+                .all(1).map(a => +a.id)))
         // todo
     }
 };
