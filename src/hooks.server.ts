ae173d0f751dab3c83a64509cf64f0d6531d7c17
z <aolose@fsm.cc>
2023-02-08 20:59:44 +0800

    handle error

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,32 +1,43 @@
-import type {Handle} from '@sveltejs/kit';
-import {contentType, encryptIv, encTypeIndex} from '$lib/enum';
-import {fwFilter, reqRLog} from "$lib/server/firewall";
-import {checkStatue, sysStatue} from "$lib/server/utils";
-import {checkRedirect} from "$lib/server/utils";
+import type { Handle, HandleServerError } from "@sveltejs/kit";
+import { contentType, encryptIv, encTypeIndex } from "$lib/enum";
+import { fwFilter, reqRLog } from "$lib/server/firewall";
+import { checkStatue, sysStatue } from "$lib/server/utils";
+import { checkRedirect } from "$lib/server/utils";
 
-checkStatue()
-export const handle: Handle = async ({event, resolve}) => {
-    const pn = event.url.pathname
-    let mk = ''
-    let res: Response | undefined
-    if (!/^\/(api|res|font|src)/.test(pn)) {
-        const p = checkRedirect(sysStatue, pn, event.request)
-        if (p) {
-            res = new Response('', {
-                status: 307,
-                headers: new Headers({
-                    location: p
-                })
-            })
-        }
-    } else {
-        mk = fwFilter(event)
-        if (mk) res = new Response('', {status: 403})
+checkStatue();
+export const handle: Handle = async ({ event, resolve }) => {
+  const pn = event.url.pathname;
+  let mk = "";
+  let res: Response | undefined;
+  if (!/^\/(api|res|font|src)/.test(pn)) {
+    const p = checkRedirect(sysStatue, pn, event.request);
+    if (p) {
+      res = new Response("", {
+        status: 307,
+        headers: new Headers({
+          location: p
+        })
+      });
     }
-    if (!res) res = await resolve(event, {
-        filterSerializedResponseHeaders: (name) =>
-            [contentType, encryptIv, encTypeIndex].indexOf(name.toLowerCase()) > -1
-    });
-    reqRLog(event, res.status, mk)
-    return res
+  } else {
+    mk = fwFilter(event);
+    if (mk) res = new Response("", { status: 403 });
+  }
+  if (!res) res = await resolve(event, {
+    filterSerializedResponseHeaders: (name) =>
+      [contentType, encryptIv, encTypeIndex].indexOf(name.toLowerCase()) > -1
+  });
+  reqRLog(event, res.status, mk);
+  return res;
 };
+
+export const handleError = (({ error, event }) => {
+  console.log("xxxx", error);
+  const { data, message ,status} = error as {
+    status:number,
+    data: string, message: string };
+  return {
+    message: message || data,
+    status:status
+  };
+})  satisfies HandleServerError;
---
+++ src/routes/+error.svelte
@@ -0,0 +1,70 @@
+<script>
+  import { page } from '$app/stores';
+  import Nav from '$lib/components/nav.svelte'
+  const error = $page.error
+  let b = ''
+</script>
+
+<div class="b" style={b}>
+  <div class="nv ">
+    <Nav/>
+  </div>
+  <div class="g">
+    <i/>
+    <h1>Oops!</h1>
+    <h3>Error {error.status}</h3>
+    <p>{error.message}</p>
+  </div>
+</div>
+<style lang="scss">
+  .nv {
+    right: 0;
+    left: 0;
+    top: 0;
+    z-index: 99;
+    position: fixed;
+  }
+  h1{
+    line-height: 2;
+    font-size: 30px;
+    font-weight: 200;
+    color: #3f5f7e;
+  }
+  h3{
+    font-weight: 200;
+    color: #fff;
+    font-size: 16px;
+  }
+  p{
+    text-align: center;
+    padding: 20px;
+    max-width: 50%;
+    min-width: 300px;
+    font-size: 20px;
+    color: #424b62;
+  }
+  i{
+    background: url("$lib/components/img/fav.png") center no-repeat;
+    width: 100px;
+    height: 100px;
+    display: block;
+    background-size: cover;
+  }
+  .b{
+    padding-bottom: 60px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
+
+  .g {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
