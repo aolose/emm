87e66550784ec590bc518f947126457a88ecf39d
z <aolose@fsm.cc>
2023-03-07 21:06:19 +0800

    blog config

--- src/routes/admin/setting/blog.svelte
+++ src/routes/admin/setting/blog.svelte
@@ -3,24 +3,60 @@
   import { sys } from "./sys";
   import Card from "./Card.svelte";
   import Ipt from "./Ipt.svelte";
+  import { getErr, trim } from "$lib/utils";
+  import { req } from "$lib/req";
+  import { seo } from "$lib/store";
 
   let url;
   let nm;
   let bio;
-  let msg = "test";
+  let msg;
   let ld;
+  let err = 0;
+  let act;
+  let key;
+  let desc;
   onMount(() => sys.subscribe(a => {
     nm = a.blogName;
     bio = a.blogBio;
-    url = a.blogUrl;
+    url = a.blogDomain;
+    key = a.seoKey;
+    desc = a.seoDesc;
   }));
   const save = () => {
-
+    ld = 1;
+    const o = {
+      seoKey: key,
+      seoDesc: desc,
+      blogName: nm, blogBio: bio, blogDomain: url
+    };
+    req("sys", o).then(() => {
+      act = 1;
+      sys.update(a => ({ ...a, ...o }));
+      err = 0;
+      msg = "update success";
+      seo.update(a => ({ ...a, title: nm, key: key, desc: desc }));
+    }).catch(e => {
+      act = 1;
+      err = 1;
+      msg = getErr(e);
+    }).finally(() => ld = 0);
   };
+  $:{
+    url = trim(url);
+    nm = trim(nm);
+    bio = trim(bio);
+    desc = trim(desc, true);
+    key = trim(key);
+    if (act) {
+      setTimeout(() => act = 0, 2e3);
+    }
+  }
 </script>
-<Card act={1} {msg} err={0} title="Blog" {save} {ld}>
+<Card {act} {msg} {err} title="Blog" {save} {ld}>
   <Ipt label="Domain" bind:value={url} placeholder="www.blog.example" />
   <Ipt label="Name" bind:value={nm} placeholder="my blog" />
-  <Ipt label="Bio" bind:value={url} placeholder="www.blog.example" />
-  <Ipt label="Domain" bind:value={url} placeholder="www.blog.example" />
+  <Ipt label="Bio" bind:value={bio} placeholder="www.blog.example" />
+  <Ipt label="Keywords" bind:value={bio} placeholder="photos,foods,.ect" />
+  <Ipt box label="Description" bind:value={bio} placeholder="description for seo" />
 </Card>
