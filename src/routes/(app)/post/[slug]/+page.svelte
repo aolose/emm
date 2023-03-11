262258591592a34c6e78b5c4ddc99f0f4aa430e3
z <aolose@fsm.cc>
2023-03-11 10:58:24 +0800

    ui

--- src/routes/(app)/post/[slug]/+page.svelte
+++ src/routes/(app)/post/[slug]/+page.svelte
@@ -16,7 +16,7 @@
 	let sly = '';
 	let style;
 	$: {
-		if (d.createAt) style = ` background: linear-gradient(rgba(0,0,0,.7),${bgColor(d.createAt)});`;
+		if (d.createAt) style = ` background: linear-gradient(rgba(0,0,0,.7),${bgColor(d.createAt)} 30%);`;
 		if (d.banner) {
 			sly = `background-image:url(/res/${d.banner})`;
 		}
