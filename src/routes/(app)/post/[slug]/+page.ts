9f2a8e7a13226b74c6fa933d1446eb4c586c3a42
z <aolose@fsm.cc>
2023-03-15 13:12:49 +0800

    fix

--- src/routes/(app)/post/[slug]/+page.ts
+++ src/routes/(app)/post/[slug]/+page.ts
@@ -7,7 +7,7 @@ export const load = apiLoad('post', ({ params: { slug } }) => slug, {
   method: method.GET,
   async done(result) {
     const p = result as Post
-    const lang = new Set((p.content.match(/(?:```)([a-z0-9]+)/g)||[]).map(a=>a.slice(3)))
-    if(lang)await regHjs(lang)
+    const lang = new Set((p.content.match(/```[a-z0-9]+/g)||[]).map(a=>a.slice(3)))
+    if(lang.size)await regHjs(lang)
   }
 });
