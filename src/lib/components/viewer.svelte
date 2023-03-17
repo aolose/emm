1de84ee3ec357694d275319560f85133810c42a0
z <aolose@fsm.cc>
2023-03-18 00:48:04 +0800

    fix:content not update when navigate at client side

--- src/lib/components/viewer.svelte
+++ src/lib/components/viewer.svelte
@@ -27,9 +27,11 @@
 				/<(h\d) id="(.+?)">(.+?)<\/\1>/g,
 				'<a class=\'head\' href="#$2" id="$2"><$1>$3</$1></a>'
 			);
-	if (!preview) {
-		title = ctx.title;
-		content = ctx.content;
+	$:{
+		if (!preview) {
+			title = ctx.title;
+			content = ctx.content;
+		}
 	}
 	const md = () => fx(`${marked.parse(content || '')}`);
 	let v = md();
