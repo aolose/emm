1663c039569c936de596a740b820797ec6da4726
z <aolose@fsm.cc>
2023-03-17 14:17:08 +0800

    fix text

--- src/routes/admin/firewall/pop.svelte
+++ src/routes/admin/firewall/pop.svelte
@@ -99,7 +99,7 @@
 						<input bind:value={d.status} />
 					</label>
 					<label transition:slide>
-						<span>times per hour:</span>
+						<span title="times per hour">TPH:</span>
 						<input bind:value={d.times} />
 					</label>
 				{:else}
