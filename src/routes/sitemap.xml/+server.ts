c216ec1c1545e5c5a4ae48bc132b0ad69b39cd58
z <aolose@fsm.cc>
2023-03-17 21:16:21 +0800

    fix header

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -3,7 +3,6 @@ import { sitemap } from "$lib/sitemap";
 
 export const GET:RequestHandler=({url})=>{
   const headers = {
-    'Cache-Control': `max-age=0, s-max-age=${600}`,
     'Content-Type': 'application/xml'
   };
   return new Response(sitemap.render(url.origin),{headers})
