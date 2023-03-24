a33b534e651d48f44a00c486c138485e4bbc4f5c
tom <i@aolose.cc>
2023-03-24 14:18:00 +0800

    ui

--- src/routes/manifest.json/+server.ts
+++ src/routes/manifest.json/+server.ts
@@ -1,38 +1,33 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { sys } from '$lib/server';
 export const GET: RequestHandler = () => {
+	const icons = [48,72,96,128,192,384,512].map(a=>{
+		return 	{
+			src: `/i${a}.png`,
+			sizes: `${a}x${a}`,
+			type: 'image/png',
+			purpose: "any maskable"
+		}
+	})
 	const data = {
 		$schema: 'https://json.schemastore.org/web-manifest-combined.json',
 		name: sys.blogName,
 		short_name: sys.blogName,
 		start_url: '/',
 		display: 'standalone',
-		background_color: '#0f1319',
+		background_color: '#000',
 		scope: '/',
-		theme_color: '#0f1319',
+		theme_color: '#000',
 		description: sys.seoDesc,
 		shortcuts: [
 			{
 				name: sys.blogName,
 				short_name: sys.blogName,
 				description: sys.seoDesc,
-				icons: [
-					{
-						src: '/i192.png',
-						sizes: '192x192',
-						purpose: "any maskable"
-					}
-				]
+				icons: icons
 			}
 		],
-		icons: [48,72,96,128,192,384,512].map(a=>{
-			return 	{
-				src: `i${a}.png`,
-				sizes: `${a}x${a}`,
-				type: 'image/png',
-				purpose: "any maskable"
-			}
-		})
+		icons: icons
 	};
 	const headers = {
 		'Content-Type': 'text/plain'
