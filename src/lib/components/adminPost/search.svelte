44a597d5f5e4339af95124df9bc2bd3f81fc7d6b
z <aolose@fsm.cc>
2023-03-28 01:21:52 +0800

    ui

--- src/lib/components/adminPost/search.svelte
+++ src/lib/components/adminPost/search.svelte
@@ -23,7 +23,7 @@
 	<i class="icon i-filter" class:act={ctx.value?.length} on:click={ctx.show} />
 	<input bind:value placeholder="search..."/>
 	<div class="l" />
-	<button class="icon i-search" class:act={value} />
+	<i class="icon i-search" class:act={value} />
 	<Filter bind:ctx />
 </div>
 
@@ -31,52 +31,44 @@
 	@import '../../break';
 
 	.a {
-		background: rgba(0, 0, 0, 0.2);
+		background: rgba(0, 0, 0, 0.24);
 		display: flex;
 		width: 300px;
 		margin: 20px auto;
 		height: 40px;
-
+    border-radius: 4px;
 		align-items: center;
 		@include s() {
 			width: 80%;
 		}
 	}
 
-
-
-	.i-filter{
-		color: #7889ab;
-		background: rgba(80,100,150,.2);
-		border-radius: 4px 0 0 4px;
-		line-height:  40px;
-		padding:  0 10px;
-	}
 	input {
 		flex: 1;
-		height: 40px;
+		padding: 0;
 		outline: none;
 		border: none;
 		&::placeholder{
 			color: #3b5572;
 		}
 	}
-	button {
-		opacity: 0.9;
-		transition: 0.2s;
-		margin-left: 4px;
-		border-radius: 0 4px 4px 0;
-		height: 40px;
+	.icon {
+		top: 1px;
+		font-size: 16px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		transition: 0.3s;
 		width: 40px;
-		background: rgba(80,100,150,.2);
-		color: #7889ab;
+		line-height: 1;
+		background: transparent;
+		color: #4b6c91;
 		cursor: pointer;
 		&:hover {
 			opacity: 1;
 		}
 	}
 	.act{
-		background: rgba(80,100,150,.5);
 		color: var(--blue);
 	}
 </style>
