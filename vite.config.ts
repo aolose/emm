4aa56343d4458a14c0f66bf3e91ae4f96b529cc8
Tom Z <anytable@gmail.com>
2025-01-28 20:02:15 +0800

    feat: add prompt for service worker update

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,22 @@
 // @ts-ignore
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			devOptions: {
+				enabled: true,
+				type: 'module'
+			},
+			strategies: 'generateSW',
+			srcDir: 'src',
+			filename: 'sw.js',
+			injectRegister: null
+		})
+	]
 };
 
 export default config;
