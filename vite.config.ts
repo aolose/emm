22c86246af4f3ced82ec343bd65bd7834cf59f0c
Tom Z <anytable@gmail.com>
2025-01-31 21:05:05 +0800

    chore: vite cfg

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	build: {
-		minify: false
-	},
 	plugins: [sveltekit()]
 };
 
