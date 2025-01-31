0dbda5002dc0599b236198935a7af335ad7ba4b4
Tom Z <anytable@gmail.com>
2025-01-31 12:57:08 +0800

    build on win

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -17,51 +17,51 @@
 </div>
 
 <style lang="scss">
-  @use 'sass:color';
-  @use '../../lib/break' as *;
+	@use 'sass:color';
+	@use '../../lib/break' as *;
 
-  :global {
-    * {
-      color: #8d9cb5;
-    }
-    *::-webkit-scrollbar-thumb {
-      border-radius: 10px;
-      background-color: color.adjust(#222f48, $alpha: -0.4);
-    }
-  }
+	:global {
+		* {
+			color: #8d9cb5;
+		}
+		*::-webkit-scrollbar-thumb {
+			border-radius: 10px;
+			background-color: color.adjust(#222f48, $alpha: -0.4);
+		}
+	}
 
-  .nv {
-    right: 0;
-    left: 0;
-    top: 0;
-    z-index: 99;
-    position: fixed;
-  }
+	.nv {
+		right: 0;
+		left: 0;
+		top: 0;
+		z-index: 99;
+		position: fixed;
+	}
 
-  .b {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    transition: 0.3s ease-in-out;
-    background-size: cover;
-  }
+	.b {
+		position: absolute;
+		left: 0;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		transition: 0.3s ease-in-out;
+		background-size: cover;
+	}
 
-  .g {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    transition: 0.3s ease-in-out;
+	.g {
+		position: absolute;
+		left: 0;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		transition: 0.3s ease-in-out;
 
-    &.ex {
-      :global {
-        .ctx {
-          padding-top: 30px;
-        }
-      }
-    }
-  }
+		&.ex {
+			:global {
+				.ctx {
+					padding-top: 30px;
+				}
+			}
+		}
+	}
 </style>
