b47cbe08c4e4d7ec95b689480f2b2598077058c6
Tom Z <anytable@gmail.com>
2026-06-10 03:27:46 +0800

    fix: restore missing method/group fields in posts page

--- src/routes/(app)/posts/[[page]]/+page.ts
+++ src/routes/(app)/posts/[[page]]/+page.ts
@@ -9,7 +9,7 @@ const baseLoad = apiLoad(
 		if (!a) error(404);
 		else return { page: a, size: 12 };
 	},
-	cache: 1e3 * 60 * 10 }
+	{ method: method.GET, group: 'posts', cache: 1e3 * 60 * 10 }
 );
 
 const hash = (s: string) => {
