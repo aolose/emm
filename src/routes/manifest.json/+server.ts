ef33ba1333c4c76f4950851d89a7065f375a7308
Tom Z <7620293+aolose@users.noreply.github.com>
2026-06-16 23:24:51 +0800

    chore: update manifest

--- src/routes/manifest.json/+server.ts
+++ src/routes/manifest.json/+server.ts
@@ -4,16 +4,8 @@ import { sys } from '$lib/server';
 const CACHE_CONTROL = 'max-age=3600';
 
 export const GET: RequestHandler = ({ request }) => {
-	const icons = [48, 72, 96, 128, 192, 384, 512].map((a) => {
-		const o = {
-			src: `/i${a}.png`,
-			sizes: `${a}x${a}`,
-			type: 'image/png'
-		} as { purpose?: string };
-		if (a === 512) o.purpose = 'maskable';
-		return o;
-	});
 	const data = {
+		id: 'emm-pwa-app',
 		name: sys?.blogName,
 		short_name: sys?.blogName,
 		start_url: '/',
@@ -22,13 +14,39 @@ export const GET: RequestHandler = ({ request }) => {
 		scope: '/',
 		theme_color: '#000',
 		description: sys?.seoDesc,
+		screenshots: [
+			{
+				src: '/home.webp',
+				sizes: '1280x720',
+				type: 'image/webp',
+				form_factor: 'wide',
+				label: 'Home'
+			},
+			{
+				src: '/home_m.webp',
+				sizes: '750x1334',
+				type: 'image/webp',
+				form_factor: 'narrow',
+				label: 'H5 Home'
+			}
+		],
 		shortcuts: [
 			{
 				name: 'Write',
-				url: '/admin'
+				url: '/admin',
+				icons: [{ src: '/i96.png', sizes: '96x96', type: 'image/png' }]
 			}
 		],
-		icons: icons
+		icons: [
+			{ src: '/i48.png', sizes: '48x48', type: 'image/png' },
+			{ src: '/i72.png', sizes: '72x72', type: 'image/png' },
+			{ src: '/i96.png', sizes: '96x96', type: 'image/png' },
+			{ src: '/i128.png', sizes: '128x128', type: 'image/png' },
+			{ src: '/i192.png', sizes: '192x192', type: 'image/png' },
+			{ src: '/i384.png', sizes: '384x384', type: 'image/png' },
+			{ src: '/i512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
+			{ src: '/i512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+		]
 	};
 	const json = JSON.stringify(data) || '';
 	const etag = `"${Bun.hash(json).toString(36)}"`;
